05998a7aa7a65369ed1e849194da4bf3ceecf26b
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-10-09 23:51:34 +0500

    funcs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,59 @@ use actix_web::{
     web::Path
 };
 
+use rhai::Engine;
+
+#[get("/add/{num1}/{num2}")]
+async fn add(path : Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
+
+    format!("{result}")
+}
+
+#[get("/multiply/{num1}/{num2}")]
+async fn add(path : Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/multiply.rhai".into()).unwrap();
+
+    format!("{result}")
+}
+
+#[get("/divide/{num1}/{num2}")]
+async fn add(path : Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/divide.rhai".into()).unwrap();
+
+    format!("{result}")
+}
+
+#[get("/subtract/{num1}/{num2}")]
+async fn add(path : Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/subtract.rhai".into()).unwrap();
+
+    format!("{result}")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
