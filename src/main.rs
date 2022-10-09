e39d6d1949ec5f17832bb52f8b06daabbddb582e
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-10-09 23:31:00 +0500

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    HttpServer,
+    App,
+    get,
+    Responder,
+    web::Path
+};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(multiply)
+            .service(add)
+            .service(subtract)
+            .service(divide)
+    })
+        .bind(("127.0.0.1", 8080))
+        .unwrap()
+        .run()
+        .await
 }
